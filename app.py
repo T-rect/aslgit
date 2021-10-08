088186b721eaf117373998b82892ec8db42ce180
T-rect <akhriza@stimata.ac.id>
2021-10-08 15:30:03 +0700

    file besar

---
+++ app.py
@@ -0,0 +1,61 @@
+from flask import Flask, render_template, request, send_from_directory
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.image import img_to_array, load_img
+from tensorflow import expand_dims
+import numpy as np
+import os
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = './static/uploads/'
+
+class_dict = {0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8:'8', 9: '9'}
+
+def predict_label(img_path, model, nama_model):
+    loaded_img = load_img(img_path, target_size=(400, 400, 3))
+    img_array = img_to_array(loaded_img) / 255.0
+    img_array = expand_dims(img_array, 0)
+    pred = model.predict(img_array)
+    prediksi = np.argmax(pred)
+    print(nama_model, ' memprediksi: ', prediksi)
+    predicted_bit = np.round(prediksi).astype('int')
+    return class_dict[predicted_bit]
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    nama_model = ''
+    model = load_model('model_MLNN.h5')
+    value_dari_indexhtml = [x for x in request.form.values()]
+    angka_sebenarnya, model_dl = value_dari_indexhtml
+    #print(angka_sebenarnya, model_dl)
+    if (model_dl == 'MLNN'):
+        nama_model = 'MLNN'
+        model = load_model('model_MLNN.h5')
+    if (model_dl == 'CNN'):
+        nama_model = 'CNN'
+        model = load_model('model_CNN.h5')
+    if (model_dl == 'CNNDO'   ):
+        nama_model = 'CNN with DO'
+        model = load_model('model_CNNDO.h5')
+    print('modelnya: ', nama_model, 'summary: ',  model.summary())
+
+    if request.method == 'POST':
+        if request.files:
+            image = request.files['image']
+            img_path = os.path.join(app.config['UPLOAD_FOLDER'], image.filename)
+            image.save(img_path)
+            image.close()
+            prediction = predict_label(img_path, model, nama_model)
+            print(img_path)
+            akurat = 'Akurat, sama dengan angka sebenarnya ' + angka_sebenarnya
+            if str(angka_sebenarnya) != prediction:
+                akurat = 'Meleset, angka sebenarnya ' + angka_sebenarnya
+            return render_template('index.html', uploaded_image=image.filename, prediction=prediction, model=nama_model, akurat=akurat)
+
+    return render_template('index.html')
+
+@app.route('/display/<filename>')
+def send_uploaded_image(filename=''):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+if __name__ == '__main__':
+    app.run(debug=True)
